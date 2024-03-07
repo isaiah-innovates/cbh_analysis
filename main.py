8cafb070a6a7e4cf3075c1cf33bf359542d7c0c4
isaiah-innovates <128506314+isaiah-innovates@users.noreply.github.com>
2024-03-07 06:58:50 -0500

    Added lyft take options

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     months = 12  # Duration of the simulation in months
     initial_riders = 1000  # Number of initial riders in the system
     initial_drivers = 100  # Number of initial drivers in the system
-    lyft_take_options = [3, 4, 5, 6]  # Different values of Lyft's take to test
+    lyft_take_options = [2.50, 3, 4, 5, 6]  # Different values of Lyft's take to test
 
     # Create and run the simulation
     simulation = Simulation(months=months, initial_riders=initial_riders, initial_drivers=initial_drivers,
